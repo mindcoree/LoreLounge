084822b722316cfd084bb44eb9a4c895eda61a70
mindcoree <cardinall1304@gmail.com>
2026-05-06 19:59:38 +0500

    refactor: изменить метод получения сессии на session_getter для улучшения читаемости

--- services/profile/src/api/dependencies/auth.py
+++ services/profile/src/api/dependencies/auth.py
@@ -3,10 +3,14 @@ from uuid import UUID
 from typing import Annotated
 
 def get_current_user(x_user_id: str = Header(..., description="ID user from KrakenD")) -> UUID:
-    if not x_user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid user ID format in header 'X-User-ID'")
-    return UUID(x_user_id)
-
+    try:
+        return UUID(x_user_id)
+    except ValueError:
+        # If the format is incorrect, we respond with a nice 401
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Invalid user ID format in header 'X-User-ID'"
+        )
 
 GuardDep = Annotated[UUID, Depends(get_current_user)]
 
--- services/profile/src/api/dependencies/session.py
+++ services/profile/src/api/dependencies/session.py
@@ -4,6 +4,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 # Импортируем наш helper из слоя инфраструктуры
 from infrastructure.db.db_helper import db_helper
- 
+
 # Создаем FastAPI зависимость
-SessionDep = Annotated[AsyncSession, Depends(db_helper.get_session)]
+SessionDep = Annotated[AsyncSession, Depends(db_helper.session_getter)]
